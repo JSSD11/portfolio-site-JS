ed73402271c05ec6cbde9a4b3bd0403f96b31d94
JSSD11 <joanne.saad@gmail.com>
2020-05-18 11:17:23 +0100

    Update entire website incluing addition of js

---
+++ js/scripts-mp.js
@@ -0,0 +1,206 @@
+
+//Slick slider
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    arrows : false,
+    cssEase: 'linear',
+    autoplay: true
+  });
+});
+
+//PhotoSwipe
+var openPhotoSwipe1 = function() {
+  var pswpElement = document.querySelectorAll('.pswp')[0];
+
+  // build items array
+  var items = [
+      {
+          src: 'img/monstapursuit/strava@1x.jpg',
+          w: 1200,
+          h: 2195
+      },
+      {
+          src: 'img/monstapursuit/endomondo@1x.jpg',
+          w: 1200,
+          h: 2195
+      },
+      {
+          src: 'img/monstapursuit/zombiesrun@1x.jpg',
+          w: 1200,
+          h: 2195
+      },
+      {
+          src: 'img/monstapursuit/goosechase@1x.jpg',
+          w: 1200,
+          h: 2195
+      },
+      {
+          src: 'img/monstapursuit/geocaching@1x.jpg',
+          w: 1200,
+          h: 2195
+      }
+  ];
+
+  // define options (if needed)
+  var options = {
+           // history & focus options are disabled on CodePen
+      history: false,
+      focus: false,
+      bgOpacity: 0.75,
+      showAnimationDuration: 0,
+      hideAnimationDuration: 0
+  };
+
+  var gallery1 = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+  gallery1.init();
+};
+
+document.getElementById("analysis").onclick=openPhotoSwipe1;
+
+var openPhotoSwipe2 = function() {
+  var pswpElement = document.querySelectorAll('.pswp')[0];
+
+  // build items array
+  var items = [
+      {
+          src: 'img/monstapursuit/prototype_1.jpg',
+          w: 1401,
+          h: 699
+      },
+      {
+          src: 'img/monstapursuit/prototype_2.jpg',
+          w: 1401,
+          h: 700
+      },
+      {
+          src: 'img/monstapursuit/prototype_3.jpg',
+          w: 1401,
+          h: 700
+      }
+  ];
+
+  // define options (if needed)
+  var options = {
+      history: false,
+      focus: false,
+      bgOpacity: 0.75,
+      showAnimationDuration: 0,
+      hideAnimationDuration: 0
+  };
+
+  var gallery2 = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+  gallery2.init();
+};
+
+document.getElementById("prototype").onclick=openPhotoSwipe2;
+
+var openPhotoSwipe3 = function() {
+  var pswpElement = document.querySelectorAll('.pswp')[0];
+
+  // build items array
+  var items = [
+      {
+          src: 'img/monstapursuit/style_guide/style-guide-01.jpg',
+          w: 1626,
+          h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-02.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-03.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-04.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-05.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-06.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-07.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-08.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-09.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-10.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-11.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-12.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-13.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-14.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-15.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-16.jpg',
+        w: 1626,
+        h: 1126
+      },
+      {
+        src: 'img/monstapursuit/style_guide/style-guide-17.jpg',
+        w: 1626,
+        h: 1126
+      }
+  ];
+
+  // define options (if needed)
+  var options = {
+      history: false,
+      focus: false,
+      bgOpacity: 0.75,
+      showAnimationDuration: 0,
+      hideAnimationDuration: 0
+  };
+
+  var gallery3 = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+  gallery3.init();
+};
+
+document.getElementById("style-guide-lbox").onclick=openPhotoSwipe3;
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,66 @@
-console.log("hello world!");
+function menuToggle() {
+  document.getElementById("mobile-nav").style.width = "100%";
+  var x = document.getElementById('hamburger');
+  //console.log(x.className);
+  if (x.className === 'icon') {
+    x.className += ' active';
+  } else {
+    x.className = 'icon';
+    document.getElementById("mobile-nav").style.width = "0%";
+  }
+}
+
+function changeLogo(){
+  var body=document.getElementsByTagName("body")[0];
+  //console.log(body.id);
+  if (body.id=="index"){
+    console.log("true");
+    var image = document.getElementsByClassName("logo")[0];
+    image.src="img/JS_logo_green.svg";
+  } else {
+    console.log("false");
+    var image = document.getElementsByClassName("logo")[0];
+    image.src="img/JS_logo_black.svg";
+  }
+}
+
+window.onload = changeLogo();
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  
