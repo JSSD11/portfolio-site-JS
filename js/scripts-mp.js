b245a6d09cf8b18ee2e23b8543be30d2da7f6fff
JSSD11 <joanne.saad@gmail.com>
2020-05-29 10:59:12 +0100

    Additions/improvements to Monstapursuit page

--- js/scripts-mp.js
+++ js/scripts-mp.js
@@ -1,3 +1,56 @@
+//hide reveal header
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('#header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    var headerHeight = document.getElementById("header").offsetHeight;
+    var documentTitle= document.title;
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('#header').removeClass('nav-down').addClass('nav-up');
+        document.getElementsByClassName('nav-up')[0].style.top = - headerHeight - 5 + "px";
+        if (window.matchMedia("(min-width: 640px)").matches && documentTitle=="Deck Drills Project"){
+              document.getElementById("horiz-anchor-nav").style.top = "0";
+              document.getElementById("horiz-anchor-nav").style.transitionDuration = "0.25s";
+            }
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('#header').removeClass('nav-up').addClass('nav-down');
+            document.getElementsByClassName('nav-down')[0].style.top ="0";
+            if (window.matchMedia("(min-width: 640px)").matches && documentTitle=="Deck Drills Project"){
+                console.log("deck drills page");
+                 document.getElementById("horiz-anchor-nav").style.top = headerHeight + "px";
+                 document.getElementById("horiz-anchor-nav").style.transitionDuration = "0.35s";
+              }
+        }
+    }
+
+    lastScrollTop = st;
+}
 
 //Slick slider
 $(document).ready(function(){
